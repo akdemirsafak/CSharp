fcafdea5515fb266517fd638e57f54626dc35556
Şafak <58888062+akdemirsafak@users.noreply.github.com>
2022-01-22 11:12:07 +0300

    addConditions

---
+++ Conditions/Program.cs
@@ -0,0 +1,26 @@
+﻿
+int time=DateTime.Now.Hour;
+if (time>=6 && time<11)
+{
+System.Console.WriteLine("Good Morning");
+}
+else if(time<=18)
+{
+    System.Console.WriteLine("Good afternoon");
+}
+else
+{
+    System.Console.WriteLine("Good evening");
+}
+
+System.Console.WriteLine("\n******************\n");
+
+// 2. Exception
+string result=string.Empty;
+//result= (time<=18 ? "Good afternoon":"Good evening");
+
+result= time>=6 && time<11 ? "Good Morning":time<=18 ? "Good Afternoon": "Good night";
+
+System.Console.WriteLine(result);
+
+
