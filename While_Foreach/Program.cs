e95841981322e66b25c7955f0d8b603bd7e8fecb
Şafak <58888062+akdemirsafak@users.noreply.github.com>
2022-01-22 13:23:34 +0300

    add While_Foreach

---
+++ While_Foreach/Program.cs
@@ -0,0 +1,32 @@
+﻿//While
+
+//1 den n e kadar(n dahil) sayıların ortalamasını alan program
+Console.Write("Tavan Sayıyı Giriniz : ");
+int input=Convert.ToInt32(Console.ReadLine());
+int number=1;
+decimal result=0;
+while(number<=input)
+{
+    result+=number;
+    number++;
+}
+System.Console.WriteLine("Ortalama : "+result/input);
+
+//a dan z ye kadar TÜM HARFLERİ yazdıran console uygulaması
+
+char character='a';
+
+while(character<='z')
+{
+    Console.Write(character + " ");
+    character++;
+}
+
+System.Console.WriteLine("\n*********FOREACH*******\n");
+
+string[] myCars={"hyundai","maserati","bmw","mercedes","skoda"};
+
+foreach(var myCar in myCars)
+{
+    Console.WriteLine(myCar);
+}
