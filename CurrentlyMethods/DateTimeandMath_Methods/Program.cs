4e1080eb568c99a0daa90017b55cc332a87a5b2b
Şafak <58888062+akdemirsafak@users.noreply.github.com>
2022-01-23 22:48:26 +0300

    add Currently Methods

---
+++ CurrentlyMethods/DateTimeandMath_Methods/Program.cs
@@ -0,0 +1,54 @@
+﻿
+//DateTime and String Methods
+
+System.Console.WriteLine(DateTime.Now);
+System.Console.WriteLine(DateTime.Now.Date);
+System.Console.WriteLine(DateTime.Now.Year);
+System.Console.WriteLine(DateTime.Now.Month);
+System.Console.WriteLine(DateTime.Now.Day);
+System.Console.WriteLine(DateTime.Now.Hour);
+System.Console.WriteLine(DateTime.Now.Minute);
+System.Console.WriteLine(DateTime.Now.DayOfWeek);
+System.Console.WriteLine(DateTime.Now.DayOfYear);
+
+System.Console.WriteLine(DateTime.Now.ToLongDateString()); //month and day are string
+System.Console.WriteLine(DateTime.Now.ToShortDateString()); //only integer date
+
+System.Console.WriteLine(DateTime.Now.ToLongTimeString()); //saniye de içerir
+System.Console.WriteLine(DateTime.Now.ToShortTimeString()); //saat dakika
+
+System.Console.WriteLine(DateTime.Now.AddDays(3));
+System.Console.WriteLine(DateTime.Now.AddMonths(2));
+System.Console.WriteLine(DateTime.Now.AddHours(5));
+System.Console.WriteLine(DateTime.Now.AddYears(10));
+//add ticks
+
+//dateTime Formats
+
+System.Console.WriteLine(DateTime.Now.ToString("dd"));
+System.Console.WriteLine(DateTime.Now.ToString("ddd"));
+System.Console.WriteLine(DateTime.Now.ToString("dddd"));
+System.Console.WriteLine(DateTime.Now.ToString("MM"));
+System.Console.WriteLine(DateTime.Now.ToString("MMM"));
+System.Console.WriteLine(DateTime.Now.ToString("MMMM"));
+System.Console.WriteLine(DateTime.Now.ToString("yy"));
+System.Console.WriteLine(DateTime.Now.ToString("yyyy"));
+
+//Math Library
+
+System.Console.WriteLine(Math.Abs(-25));
+System.Console.WriteLine(Math.Sin(10));
+System.Console.WriteLine(Math.Cos(10));
+System.Console.WriteLine(Math.Tan(10));
+
+System.Console.WriteLine(Math.Ceiling(22.3));
+System.Console.WriteLine(Math.Round(22.3));
+System.Console.WriteLine(Math.Round(22.7));
+System.Console.WriteLine(Math.Floor(22.3));
+
+System.Console.WriteLine(Math.Max(22,5));
+System.Console.WriteLine(Math.Min(3,5));
+System.Console.WriteLine(Math.Sqrt(25));//karekök alır
+System.Console.WriteLine(Math.Log(9));//9 un e tabanındaki logaritmik karşılığı
+System.Console.WriteLine(Math.Exp(3));//e üzeri 3
+System.Console.WriteLine(Math.Log10(10));
---
+++ CurrentlyMethods/StringMethods/Program.cs
@@ -0,0 +1,51 @@
+﻿
+string word="Hello Welcome!",other="hello Welcome!";
+
+System.Console.WriteLine("Length : "+word.Length);
+System.Console.WriteLine("ToUpper : "+word.ToUpper());
+System.Console.WriteLine("ToLower : "+word.ToLower());
+System.Console.WriteLine(String.Concat(word," Concat ile Birleştirdik"));
+System.Console.WriteLine("\n******* CompareTo and Compare ********");
+//CompareTo
+System.Console.WriteLine(word.CompareTo(other)); 
+//KARAKTER SAYISI iki değişken eşitse 0 döndürür. 1. değişken  2. değişkenden büyükse 1 döndürür, 2. değişken 1. değişkenden büyükse -1 döndürür.
+//Compare
+System.Console.WriteLine(String.Compare(word,other,true));// sondaki parametre true ise non caseSensitive hale gelir
+
+System.Console.WriteLine(String.Compare(word,other,false)+"\n");// sondaki parametre false ise caseSensitive hale gelir
+
+System.Console.WriteLine("\nContains");
+System.Console.WriteLine(word.Contains("hello"));
+
+System.Console.WriteLine("\nEndsWith and StartsWith");
+System.Console.WriteLine(word.EndsWith("come!"));
+System.Console.WriteLine(word.StartsWith("Hello"));
+
+System.Console.WriteLine("\nIndexOf");
+System.Console.WriteLine(word.IndexOf("x")); //varsa ilk bulduğunun indexini döner yoksa -1 döner.
+System.Console.WriteLine(word.LastIndexOf("x")); //aramaya sondan başlar
+
+System.Console.WriteLine("\nInsert");
+System.Console.WriteLine(word.Insert(word.Length,"x")); //(index,karakter) karakteri verilen index 'e ekler.
+
+
+System.Console.WriteLine("\nPadLeft and PadRight");
+System.Console.WriteLine(word.PadLeft(30)); //eğer string ifadedeki karakter sayısı 30 dan küçükse 30 a tamamlayacak kadar sola boşluk eklenir.(ya da istediğimiz karakter)
+System.Console.WriteLine(word.PadRight(30,"*"));//yukardakinin sağa ekleyeni
+
+System.Console.WriteLine("\nRemove");
+System.Console.WriteLine(word.Remove(3,3)); //Verilen indeksten sonraki 5 karakteri siler.
+System.Console.WriteLine(word.Remove(10)); //Verilen indeksten sonraki kısmı siler.
+
+System.Console.WriteLine("\nReplace");
+System.Console.WriteLine(word.Replace("Hello","Merhaba")); //eski deger,yeni deger
+
+System.Console.WriteLine("\nSplit");
+System.Console.WriteLine(word.Split(' '[1])); //Boşluklara göre parçalara ayır 1. indexteki parçayı getir.
+
+
+System.Console.WriteLine("\nSubString");
+System.Console.WriteLine(word.Substring(4)); //4. indexten sonrasını getirir.
+
+System.Console.WriteLine(word.Substring(4,6)); //4. indexten sonra 6 karakter getirir.
+
